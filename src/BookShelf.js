9d3dddc59af9ff3ce1b8bcdac992c7c85de6209e
Dayan Petrov <dayan.petroff@gmail.com>
2018-02-16 02:50:30 +0200

    Updated a comment in BookShelf.js

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -17,11 +17,11 @@ class BookShelf extends React.Component {
         <h2 className="bookshelf-title">{this.props.data.title}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-            /** simple indication if there are no books on a given shelf */
+           
             {books.length === 0 &&
               <div>This shelf is empty</div>
             }
-            /** display books */
+          
             {books.map((book) =>
               <Book key={book.id} book={book} allBooks={allBooks} addBookTo={addBookTo}/>
             )}
